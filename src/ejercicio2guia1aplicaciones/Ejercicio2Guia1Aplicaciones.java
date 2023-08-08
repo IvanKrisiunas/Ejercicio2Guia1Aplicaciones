62f31c097164df0aaf0e690e4009f705c220d6e2
IvanKrisiunas <141675048+IvanKrisiunas@users.noreply.github.com>
2023-08-08 17:01:55 -0300

    h

--- src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
+++ src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
@@ -1,10 +1,10 @@
-
 package ejercicio2guia1aplicaciones;
 
 public class Ejercicio2Guia1Aplicaciones {
-    
+
     public static void main(String[] args) {
-        Perro p1 = new Perro("chapo", 10, "Bulldog Frances", 20,5);
+        Perro p1 = new Perro("Chapo", 5, "Bulldog Frances", 20.5);
+        p1.accion();
     }
-    
+
 }
--- src/ejercicio2guia1aplicaciones/Perro.java
+++ src/ejercicio2guia1aplicaciones/Perro.java
@@ -1,14 +1,60 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio2guia1aplicaciones;
 
-/**
- *
- * @author Monica
- */
+import java.util.Scanner;
+
 public class Perro {
+    private String nombre;
+    private int edad;
+    private String raza;
+    private double altura;
+
+    public Perro(String nombre, int edad, String raza, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.raza = raza;
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
     
+    
+    public void accion(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Si quieres que el perro ladre tienes que decir habla");
+        String resp = leer.nextLine();
+        if (resp.contentEquals("habla")) {
+            System.out.println("*" + nombre + "*" + " wau");
+            System.out.println("Aguante boca");
+        }
+    }
 }
