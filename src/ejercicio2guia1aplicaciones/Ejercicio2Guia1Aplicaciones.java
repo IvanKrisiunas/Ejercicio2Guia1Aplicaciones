a1f9693b488c49e5d9fa9caa815650ea2c830a94
eliankrisiunas <141676527+eliankrisiunas@users.noreply.github.com>
2023-08-08 16:17:04 -0300

    hola
    aloh

--- src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
+++ src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ejercicio2guia1aplicaciones;
 
-/**
- *
- * @author Monica
- */
+package ejercicio2guia1aplicaciones;
 public class Ejercicio2Guia1Aplicaciones {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        private int numero;
     }
     
 }
