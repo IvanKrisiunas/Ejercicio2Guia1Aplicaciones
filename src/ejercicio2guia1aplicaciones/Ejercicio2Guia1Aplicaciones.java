dbf8d5c44623bf89c3c84470f6027590132d0651
eliankrisiunas <141676527+eliankrisiunas@users.noreply.github.com>
2023-08-08 16:25:21 -0300

    hola que tal
    hola que tal

--- src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
+++ src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
@@ -4,7 +4,7 @@ package ejercicio2guia1aplicaciones;
 public class Ejercicio2Guia1Aplicaciones {
 private int num; 
     public static void main(String[] args) {
-        
+//        hola que tal
     }
     
 }
