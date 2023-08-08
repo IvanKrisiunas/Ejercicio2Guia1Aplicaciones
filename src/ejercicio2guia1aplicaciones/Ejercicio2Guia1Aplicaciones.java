4f5d0417173b8f4078e1b73466614214ed616203
IvanKrisiunas <141675048+IvanKrisiunas@users.noreply.github.com>
2023-08-08 16:17:30 -0300

    boca
    juniors

--- src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
+++ src/ejercicio2guia1aplicaciones/Ejercicio2Guia1Aplicaciones.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ejercicio2guia1aplicaciones;
 
-/**
- *
- * @author Monica
- */
 public class Ejercicio2Guia1Aplicaciones {
-
-    /**
-     * @param args the command line arguments
-     */
+private int num;
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
